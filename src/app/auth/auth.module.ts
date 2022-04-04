fe374018bce9e57dc1bb2f39bf7c50e7d5aa4592
Fernando Nicolas <fersanti2896@gmail.com>
2022-04-03 23:04:11 -0500

    Diseño de los componentes login y registrer.component

--- src/app/auth/auth.module.ts
+++ src/app/auth/auth.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ReactiveFormsModule } from '@angular/forms';
 
 import { AuthRoutingModule } from './auth-routing.module';
 import { LoginComponent } from './pages/login/login.component';
@@ -10,11 +11,12 @@ import { MainComponent } from './pages/main/main.component';
   declarations: [
     LoginComponent, 
     RegisterComponent, 
-    MainComponent
+    MainComponent,
   ],
   imports: [
     CommonModule,
-    AuthRoutingModule
+    AuthRoutingModule, 
+    ReactiveFormsModule
   ]
 })
 export class AuthModule { }
